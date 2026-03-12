c093997a5fe13dba5bfd55f2001ce33bc8340585
Aditya Arora <aroraaditya358@gmail.com>
2026-03-12 09:26:25 +0530

    3rd commit

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -12,41 +12,7 @@ class ReverseStringStrategy implements PalindromeStrategy {
     }
 }
 
-class StackStrategy implements PalindromeStrategy {
-    public boolean checkPalindrome(String input) {
-        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        Stack<Character> stack = new Stack<>();
 
-        for (char c : normalized.toCharArray()) {
-            stack.push(c);
-        }
-
-        for (char c : normalized.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
-class DequeStrategy implements PalindromeStrategy {
-    public boolean checkPalindrome(String input) {
-        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        Deque<Character> deque = new ArrayDeque<>();
-
-        for (char c : normalized.toCharArray()) {
-            deque.add(c);
-        }
-
-        while (deque.size() > 1) {
-            if (!deque.pollFirst().equals(deque.pollLast())) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
@@ -55,22 +21,15 @@ public class PalindromeCheckerApp {
         String input = sc.nextLine();
 
         PalindromeStrategy reverseStrategy = new ReverseStringStrategy();
-        PalindromeStrategy stackStrategy = new StackStrategy();
-        PalindromeStrategy dequeStrategy = new DequeStrategy();
+
 
         boolean result1 = reverseStrategy.checkPalindrome(input);
 
-        boolean result2 = stackStrategy.checkPalindrome(input);
 
-        boolean result3 = dequeStrategy.checkPalindrome(input);
 
         System.out.println("Input : " + input);
 
         System.out.println("Reverse Strategy Result : " + result1);
-
-        System.out.println("Stack Strategy Result : " + result2);
-
-        System.out.println("Deque Strategy Result : " + result3);
 
     }
 }
